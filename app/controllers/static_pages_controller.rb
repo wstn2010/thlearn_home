f1fe9ab9be810520df25f26b2dc1d5d0936f3d9d
kenputer <wstn2010@gmail.com>
2015-05-05 13:19:44 +0900

    resolved #8 : top画面

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,14 @@
 class StaticPagesController < ApplicationController
 
   def home
-    if signed_in?
-      @micropost  = current_user.microposts.build
-      @feed_items = current_user.feed.paginate(page: params[:page])
-    end
+    @apps = 
+    [
+      {:url=>"/master.jsp", :title=>"会計基準マスター", :desc=>"会計基準ごとに仕上げていきます"},
+      {:url=>"/exam.jsp", :title=>"会計基準穴埋め実力テスト", :desc=>"<font color=red>New!</font> 穴埋め問題で定着度を確認しましょう"},
+      {:url=>"http=>//diyeda.appspot.com/", :title=>"財務諸表論 超短答演習ツール", :desc=>"<font color=red>New!</font> 丸暗記はやめましょう。試験後の実力維持にも"},
+      {:url=>"/app.jsp", :title=>"財務諸表論ワークブック(穴埋式)", :desc=>"<a href='/app.jsp?anony=true'>ログインしないでスタート</a>"},
+      {:url=>"/app.jsp?selectMode=true", :title=>"財務諸表論ワークブック(選択式)", :desc=>"<a href='/app.jsp?selectMode=true&anony=true'>ログインしないでスタート</a>"}
+    ]
   end
 
   def help
@@ -16,3 +20,13 @@ class StaticPagesController < ApplicationController
   def contact
   end
 end
+
+
+
+
+# <!-- login box --> 
+#   <div class="enter"> <div class="enter-border"> <div class="enter-innerbox"> <div class="enter-flexbluebtn"> 
+#     <div><p><span><a href="/app.jsp?selectMode=true" class="access">財務諸表論ワークブック(選択式)<br/>スタート</a></span></p></div> 
+#     <p class="enter-description"><a href="/app.jsp?selectMode=true&anony=true">ログインしないでスタート</a></p> 
+#   </div></div></div></div> 
+# <!-- end login box --> 
--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,2 +1,20 @@
 module StaticPagesHelper
+
+	def select_ad
+		n = Random.rand(0 .. 3)
+
+		p = Random.rand
+		if (p < 0.4)
+			"/assets/ad_slide0.png"
+		elsif (p < 0.7)
+			"/assets/ad_slide1.png"
+		elsif (p < 0.9)
+			"/assets/ad_slide2.png"
+		else
+			"/assets/ad_slide3.png"
+		end
+	end
+
+
 end
+
