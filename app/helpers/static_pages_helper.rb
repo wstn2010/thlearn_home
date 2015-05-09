30a7856def5fa12add08945dfc2b70d7eafe6af4
kenputer <wstn2010@gmail.com>
2015-05-09 09:02:04 +0900

    resolved #9: 説明ページを持ってきた＋トップのレイアウト整備

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -4,14 +4,14 @@ module StaticPagesHelper
 		n = Random.rand(0 .. 3)
 
 		p = Random.rand
-		if (p < 0.4)
-			"/assets/ad_slide0.png"
-		elsif (p < 0.7)
-			"/assets/ad_slide1.png"
+		if (p < 0.5)
+			{img: "/assets/ad_slide0.png", url: "http://itunes.apple.com/ja/app/pocket-accounting-standard/id581530923"}
+		elsif (p < 0.75)
+			{img: "/assets/ad_slide1.png", url: "http://itunes.apple.com/jp/app/flex-accounting-standards/id483565955"}
 		elsif (p < 0.9)
-			"/assets/ad_slide2.png"
+			{img: "/assets/ad_slide2.png", url: "http://itunes.apple.com/jp/app/da-liantaima/id517461745"}
 		else
-			"/assets/ad_slide3.png"
+			{img: "/assets/ad_slide3.png", url: "http://itunes.apple.com/jp/app/id435907634"}
 		end
 	end
 
