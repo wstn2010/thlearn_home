0d5e90f4ab00420ea3bb796c581adc1a5b27e2e1
kenputer <wstn2010@gmail.com>
2015-05-09 11:39:44 +0900

    画像リンクまわり修正

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -5,13 +5,13 @@ module StaticPagesHelper
 
 		p = Random.rand
 		if (p < 0.5)
-			{img: "/assets/ad_slide0.png", url: "http://itunes.apple.com/ja/app/pocket-accounting-standard/id581530923"}
+			{img: "ad_slide0.png", url: "http://itunes.apple.com/ja/app/pocket-accounting-standard/id581530923"}
 		elsif (p < 0.75)
-			{img: "/assets/ad_slide1.png", url: "http://itunes.apple.com/jp/app/flex-accounting-standards/id483565955"}
+			{img: "ad_slide1.png", url: "http://itunes.apple.com/jp/app/flex-accounting-standards/id483565955"}
 		elsif (p < 0.9)
-			{img: "/assets/ad_slide2.png", url: "http://itunes.apple.com/jp/app/da-liantaima/id517461745"}
+			{img: "ad_slide2.png", url: "http://itunes.apple.com/jp/app/da-liantaima/id517461745"}
 		else
-			{img: "/assets/ad_slide3.png", url: "http://itunes.apple.com/jp/app/id435907634"}
+			{img: "ad_slide3.png", url: "http://itunes.apple.com/jp/app/id435907634"}
 		end
 	end
 
