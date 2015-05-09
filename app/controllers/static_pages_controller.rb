11713ffeef8d5522ad5d5401b79f0be585881e49
kenputer <wstn2010@gmail.com>
2015-05-09 11:34:40 +0900

    画像リンクまわり修正

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -6,7 +6,7 @@ class StaticPagesController < ApplicationController
       {url:"http://thlearn.appspot.com/master.jsp", title:"会計基準マスター", desc:"記憶メカニズムに基づいて穴埋め問題を出題。会計基準ごとに、クイズ形式の演習で仕上げていきます", anony:"disable"},
       {url:"http://thlearn.appspot.com/exam.jsp", title:"会計基準穴埋め実力テスト", desc:"<font color=red>New!</font> 穴埋め問題で定着度を確認しましょう。結果はe-mailで返送されます。", anony:"disable"},
       {url:"http://diyeda.appspot.com/?dummy=x", title:"財務諸表論 超短答式演習", desc:"<font color=red>New!</font> 理論の丸暗記はやめましょう。試験後の実力維持にも", anony:"only"},
-      {url:"http://thlearn.appspot.com/app.jsp?dummy=x", title:"財務諸表論ワークブック(穴埋式)", desc:"会計基準等の穴埋め問題を任意の範囲から出題し、演習するための アプリケーションです。", anony:"enable"},
+      {url:"http://thlearn.appspot.com/app.jsp?dummy=x", title:"財務諸表論ワークブック(穴埋式)", desc:"会計基準等の任意の範囲を集中して演習するためのアプリケーションです。", anony:"enable"},
       {url:"http://thlearn.appspot.com/app.jsp?selectMode=true", title:"財務諸表論ワークブック(選択式)", desc:"学習初期に適した選択式バージョンです。", anony:"enable"}
     ]
   end
